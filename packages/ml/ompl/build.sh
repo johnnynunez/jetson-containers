542626d8bf928fee18bfac613919db696e9da80d
johnnynunez <johnnynuca14@gmail.com>
2025-01-15 03:38:49 +0100

    fix

--- packages/ml/ompl/build.sh
+++ packages/ml/ompl/build.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
+wget https://apt.llvm.org/llvm.sh
+chmod u+x llvm.sh
+./llvm.sh 18 all
+
 # Clone the repository if it doesn't exist
-git clone --branch=v${OMPL_VERSION} --depth=1 --recursive https://github.com/ompl/ompl /opt/ompl || \
-git clone --depth=1 --recursive https://github.com/ompl/ompl /opt/ompl
+wget https://ompl.kavrakilab.org/install-ompl-ubuntu.sh
+chmod u+x install-ompl-ubuntu.sh
+./install-ompl-ubuntu.sh -g -p
 
-# Navigate to the directory containing ompl's setup.py
-cd /opt/ompl 
 
-pip3 install 'numpy<2' pyplusplus pygccxml
-pip3 install 'numpy<2'
-
-export MAX_JOBS=$(nproc)
-mkdir -p build/Release
-cd build/Release
-cmake ../..
-# next step is optional
-make -j ${MAX_JOBS} update_bindings # if you want Python bindings
-make -j ${MAX_JOBS}
-make install
-pip3 wheel --no-build-isolation --wheel-dir=/opt/ompl/wheels .
-pip3 install --no-cache-dir --verbose /opt/ompl/wheels/ompl_ssm*.whl
-
-cd /opt/ompl
 pip3 install 'numpy<2'
 
 # Optionally upload to a repository using Twine
-twine upload --verbose /opt/ompl/wheels/ompl_ssm*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/ompl/wheels/ompl*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
--- packages/ml/ompl/install.sh
+++ packages/ml/ompl/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
+wget https://apt.llvm.org/llvm.sh
+chmod u+x llvm.sh
+./llvm.sh 18 all
+
 if [ "$FORCE_BUILD" == "on" ]; then
 	echo "Forcing build of ompl ${OMPL}"
 	exit 1
